3638f9db17eb025ee011108fe06610e2e7e9bdc0
Guilherme <guilherme.silva.k@gmail.com>
2023-04-24 18:30:16 -0300

    listaAtualizavel

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import Item from './Item';
 import style from './List.module.scss';
 
 function List(){
 
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: 'React',
         tempo: '02:02:50'
     },{
@@ -12,12 +13,14 @@ function List(){
     },{
         tarefa: 'Nodejs',
         tempo:'05:00:00'
-    }]
+    }])
 
     return(
 
         <aside className={style.listaTarefas}>
-            <h2>Estudos do Dia</h2>
+            <h2 onClick={() => {
+                    setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}])
+            }}>Lista de Tarefas:</h2>
             <ul>
             {tarefas.map((item,index) => (
                 <Item 
